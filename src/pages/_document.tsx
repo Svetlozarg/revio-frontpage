8e669dd6f6d93404d2bd4fb8f0fbb9a9871a7268
Svetlozar Georgiev <s.georgiev01@proton.me>
2023-09-14 16:25:53 +0300

    Revio

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FLKBGD5V9Z"
+          async
+        />
+        <Script id="ga-script">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FLKBGD5V9Z');
+            `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
